781e30534c5ae7bea538278b8a3eb9ee973ddbb1
Supertraced <65104199+Supertrace@users.noreply.github.com>
2020-12-10 00:36:14 -0700

    clean up/slight optimization

--- Day10/Day10Prob2.hs
+++ Day10/Day10Prob2.hs
@@ -8,15 +8,12 @@ import Data.List
 sorter :: [String] -> [Int]
 sorter lst = reverse $ (0 : (sort $ map read lst))
 
-reachable :: [Int] -> Int
-reachable (a:rest) = length $ takeWhile (<= a-3) rest
-
 chainCount :: [(Int, Int)] -> [Int] -> [(Int,Int)]
 chainCount kVals []           = kVals
 chainCount kVals lst@(a:rest) = chainCount newVal rest where
   aVal   = case kVals of
              [] -> 1
-             _  -> sum $ map snd $ filter ((<= a + 3) . fst) kVals
+             _  -> sum $ map snd $ takeWhile ((<= a + 3) . fst) kVals
   newVal = (a, aVal) : kVals
 
 main :: IO()
