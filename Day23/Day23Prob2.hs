de44e8fd54d57859799134bc3676ca8a0de8e6b9
Supertraced <65104199+Supertrace@users.noreply.github.com>
2020-12-23 12:38:25 -0700

    clean up

--- Day23/Day23Prob2.hs
+++ Day23/Day23Prob2.hs
@@ -14,13 +14,13 @@ makeMove :: (Int,Circle) -> (Int,Circle)
 makeMove (curr, circ) = (newLoc, resultC) where
   (prev,x) = circ M.! curr
   [a,b,c]  = cTake 3 x circ
-  moveTo = goal curr [a,b,c]
-  newLoc = last $ cTake 2 c circ
-  nCirc   = M.insert newLoc (curr, last $ cTake 2 newLoc circ) $ M.insert curr (prev, newLoc) circ
+  moveTo   = goal curr [a,b,c]
+  newLoc   = last $ cTake 2 c circ
+  nCirc    = M.insert newLoc (curr, last $ cTake 2 newLoc circ) $ M.insert curr (prev, newLoc) circ
   (pMoveTo,aftBl) = case nCirc M.!? moveTo of
                       Just a -> a
                       _      -> (moveTo-1,moveTo+1)
-  aaftBl = last $ cTake 2 aftBl nCirc
+  aaftBl  = last $ cTake 2 aftBl nCirc
   resultC = M.insert aftBl (c, aaftBl) $ M.insert c (b,aftBl) $ M.insert a (moveTo,b) $ M.insert moveTo (pMoveTo, a) nCirc 
 
 cTake :: Int -> Int -> Circle -> [Int]
@@ -49,17 +49,17 @@ mapConstruct circ prev (top:rest) = mapConstruct nCirc (Just top) rest where
 mepUpdate :: M.HashMap Int v -> [(Int, v)] -> M.HashMap Int v
 mepUpdate = foldr (\(k,v) circ -> M.insert k v circ)
 
---helper :: [Int] -> (Integer, [Integer])
+helper :: [Int] -> (Integer, [Integer])
 helper input  = (result, vals) where
-  mep' = mapConstruct M.empty Nothing input
-  mep  = mepUpdate mep' [(0, (1000000-1, head input)), (1000000-1, (1000000-2,0)), (10, (last input, 11))]
-  rMep = snd $ head $ drop 10000000 $ iterate makeMove (head input, mep)
-  vals = map toInteger $ drop 1 $ cTake 3 1 rMep
+  mep'   = mapConstruct M.empty Nothing input
+  mep    = mepUpdate mep' [(0, (1000000-1, head input)), (1000000-1, (1000000-2,0)), (10, (last input, 11))]
+  rMep   = snd $ head $ drop 10000000 $ iterate makeMove (head input, mep)
+  vals   = map toInteger $ drop 1 $ cTake 3 1 rMep
   result = product vals
 
 main :: IO()
 main = do
     args <- getArgs
-    (readFile $ head args) >>= mapM_ (putStrLn . show) . helper . map (digitToInt) . head . lines   
+    (readFile $ head args) >>= putStrLn . show . helper . map (digitToInt) . head . lines   
     >> exitSuccess
 --10000000
